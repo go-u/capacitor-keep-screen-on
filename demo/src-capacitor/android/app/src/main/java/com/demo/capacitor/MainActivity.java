b92f99058dc1077aeaa41a7ef5e76103ff966b79
go-u <53477091+go-u@users.noreply.github.com>
2019-11-07 02:58:15 +0900

    add plugin

--- demo/src-capacitor/android/app/src/main/java/com/demo/capacitor/MainActivity.java
+++ demo/src-capacitor/android/app/src/main/java/com/demo/capacitor/MainActivity.java
@@ -7,6 +7,9 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
+// plugin
+import com.go.capacitor.keepscreenon.CapacitorKeepScreenOn;
+
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+       add(CapacitorKeepScreenOn.class);
     }});
   }
 }
