55613ae6f4abd4b3f37482e9d478422d74484cb6
go-u <53477091+go-u@users.noreply.github.com>
2019-11-07 02:30:52 +0900

    add demo button

--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -1,11 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center column q-gutter-y-md">
     <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <q-btn label="enable" @click="enable()"/>
+    <q-btn label="disable" @click="disable()"/>
   </q-page>
 </template>
 
 <script>
+import { Plugins } from '@capacitor/core'
+const { CapacitorKeepScreenOn } = Plugins
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  methods: {
+    enable: function (bool) {
+      CapacitorKeepScreenOn.enable()
+        .then((res) => {
+          console.log(res)
+        }).catch((err) => {
+          console.log(err)
+        })
+    },
+    disable: function (bool) {
+      CapacitorKeepScreenOn.disable()
+        .then((res) => {
+          console.log(res)
+        }).catch((err) => {
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
