8570f1d5d213eeb15b002756d70a28ce0c1ebb77
go-u <53477091+go-u@users.noreply.github.com>
2019-11-07 22:34:29 +0900

    add: enable / disable method

--- android/src/main/java/com/go/capacitor/keepscreenon/CapacitorKeepScreenOn.java
+++ android/src/main/java/com/go/capacitor/keepscreenon/CapacitorKeepScreenOn.java
@@ -6,15 +6,38 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+// added for this plugin
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
 @NativePlugin()
 public class CapacitorKeepScreenOn extends Plugin {
 
-    @PluginMethod()
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    // Keys For LoacalBroadcast
+    public static final String BROADCAST_KEY_ENABLE_KEEP_SCREEN_ON = "ENABLE_KEEP_SCREEN_ON";
+    public static final String BROADCAST_KEY_DISABLE_KEEP_SCREEN_ON = "DISABLE_KEEP_SCREEN_ON";
 
+    @PluginMethod()
+    public void enable(PluginCall call) {
+        Intent localBroadCastIntent = new Intent(BROADCAST_KEY_ENABLE_KEEP_SCREEN_ON);
+        dispatchIntent(call, localBroadCastIntent);
+    }
+
+    @PluginMethod()
+    public void disable(PluginCall call) {
+        Intent localBroadCastIntent = new Intent(BROADCAST_KEY_DISABLE_KEEP_SCREEN_ON);
+        dispatchIntent(call, localBroadCastIntent);
+    }
+
+    private void dispatchIntent(PluginCall call, Intent intent) {
+        Boolean hasDispatched = LocalBroadcastManager.getInstance(this.getContext()).sendBroadcast(intent);
         JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+        if (hasDispatched) {
+            Boolean mode = (BROADCAST_KEY_ENABLE_KEEP_SCREEN_ON.equals(intent.toString()));
+            ret.put("isEnabled", mode);
+            call.success(ret);
+        } else {
+            call.error("LocalBroadcast dispatch failed");
+        }
     }
 }
