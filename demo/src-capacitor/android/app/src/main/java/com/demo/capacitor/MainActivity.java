bc537ec6700de763ca9ea6a9bc6e87500e4e2237
go-u <53477091+go-u@users.noreply.github.com>
2019-11-07 22:39:53 +0900

    add receiver for plugin

--- demo/src-capacitor/android/app/src/main/java/com/demo/capacitor/MainActivity.java
+++ demo/src-capacitor/android/app/src/main/java/com/demo/capacitor/MainActivity.java
@@ -9,6 +9,14 @@ import java.util.ArrayList;
 
 // plugin
 import com.go.capacitor.keepscreenon.CapacitorKeepScreenOn;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.Context;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.WindowManager;
+// todo: remove this import for test dialog
+import android.app.AlertDialog;
 
 public class MainActivity extends BridgeActivity {
   @Override
@@ -21,5 +29,39 @@ public class MainActivity extends BridgeActivity {
       // Ex: add(TotallyAwesomePlugin.class);
        add(CapacitorKeepScreenOn.class);
     }});
+
+    // For KEEP_SCREEN_ON plugin
+    // Register EnableKeepScreenOnReceiver
+    if(this.enableKeepScreenOnReceiver == null) {
+      IntentFilter intentFilter = new IntentFilter(CapacitorKeepScreenOn.BROADCAST_KEY_ENABLE_KEEP_SCREEN_ON);
+      this.enableKeepScreenOnReceiver = new EnableKeepScreenOnReceiver();
+      LocalBroadcastManager.getInstance(this).registerReceiver(this.enableKeepScreenOnReceiver, intentFilter);
+    }
+    // Register DisableKeepScreenOnReceiver
+    if(this.disableKeepScreenOnReceiver == null) {
+      IntentFilter intentFilter = new IntentFilter(CapacitorKeepScreenOn.BROADCAST_KEY_DISABLE_KEEP_SCREEN_ON);
+      this.disableKeepScreenOnReceiver = new DisableKeepScreenOnReceiver();
+      LocalBroadcastManager.getInstance(this).registerReceiver(this.disableKeepScreenOnReceiver, intentFilter);
+    }
+  }
+
+  private EnableKeepScreenOnReceiver enableKeepScreenOnReceiver = null;
+  public class EnableKeepScreenOnReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+      // todo: remove this test message
+      // new AlertDialog.Builder(MainActivity.this).setTitle("plugin register test").setMessage("this is test message").setPositiveButton("OK", null).show();
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+  }
+
+  private DisableKeepScreenOnReceiver disableKeepScreenOnReceiver = null;
+  public class DisableKeepScreenOnReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+      getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
   }
 }
