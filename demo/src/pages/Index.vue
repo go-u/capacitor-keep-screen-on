533c8a5e79bbe261084bf75f608489466b5a5f0c
go-u <53477091+go-u@users.noreply.github.com>
2019-11-14 21:04:45 +0900

    added: getState() test button

--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -3,6 +3,7 @@
     <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
     <q-btn label="enable" @click="enable()"/>
     <q-btn label="disable" @click="disable()"/>
+    <q-btn label="get state" @click="getState()"/>
   </q-page>
 </template>
 
@@ -25,6 +26,14 @@ export default {
       CapacitorKeepScreenOn.disable()
         .then((res) => {
           console.log(res)
+        }).catch((err) => {
+          console.log(err)
+        })
+    },
+    getState: function () {
+      CapacitorKeepScreenOn.getState()
+        .then((res) => {
+          alert(res.isEnabled)
         }).catch((err) => {
           console.log(err)
         })
